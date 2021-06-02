358de5f1fe062a98821bb151f343803adfacd53b
Surya Aditya <suryaaditya4177@gmail.com>
2021-06-02 14:15:01 +0800

    Updates

--- src/App.js
+++ src/App.js
@@ -5,9 +5,13 @@ import Header from './pages/Header';
 import Home from './pages/Home';
 import Checkout from './pages/Checkout';
 import Login from './pages/Login';
+import Payment from './pages/Payment';
 import { auth } from './firebase';
 import { useStateValue } from './provider/StateProvider';
-import Payment from './pages/Payment';
+import { loadStripe } from '@stripe/stripe-js'
+import { Elements } from '@stripe/react-stripe-js'
+
+const promise = loadStripe('pk_test_51IUQeAH3KKLZeaeM982nEHjSiy0pn95TFv8rCVqhUmNfLqBi5qUMMT4EIpebajVsQGyRRxfOi2fxpH9bvBlDtFIV00CTNsINT5');
 
 function App() {
   const [{}, dispatch] = useStateValue();
@@ -45,7 +49,9 @@ function App() {
           </Route>
           <Route path="/payment">
             <Header/>
-            <Payment/>
+            <Elements stripe={promise}>
+              <Payment/>
+            </Elements>
           </Route>
           <Route path="/">
             <Header/>
